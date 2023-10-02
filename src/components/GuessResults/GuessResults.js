b15e3a79978fb7517db8ae9550e4f2c53ee39a7f
michael kostal <michael.kostal@united.com>
2023-10-02 10:54:14 -0500

    Add range, guesses, and grid

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
 
-function GuessResults({guesses}) {
+function Grid({ numRows, numCols }) {
+  return (
+    <div className="grid">
+      {range(numRows).map((num) => (
+        <p className="guess" key={num}>
+          {range(numCols).map((num) => (
+            <span className="cell" key={num}></span>
+          ))}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+function GuessResults({ guesses }) {
   return <div className="guess-results">
-  {guesses.map((guess, index) =>{
-    return <p className="guess" key={index}>{guess}</p>
-  })}
-</div>;
+    <Grid
+      numRows={NUM_OF_GUESSES_ALLOWED}
+      numCols={5}
+    />
+    {guesses.map((guess, index) => {
+      return <p className="guess" key={index}>{guess}</p>
+    })}
+  </div>;
 }
 
 export default GuessResults;

eea0db52411e61dde024f26b80c28603cb92cb96
michael kostal <michael.kostal@united.com>
2023-10-02 11:23:24 -0500

    After video

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,19 +1,18 @@
 import React from 'react';
+
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 
-
+import Guess from '../Guess';
 
 function GuessResults({ guesses }) {
-  return <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((rowNum) => (
-        <p className="guess" key={rowNum}>
-          {range(5).map((colNum, index) => (
-            <span className="cell" key={colNum}>{guesses[rowNum] && (guesses[rowNum]).charAt(index)}</span>
-          ))}
-        </p>
+  return (
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
+  );
 }
 
 export default GuessResults;

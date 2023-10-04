28977f0066b5a65caa4f17a883aa32707b0d9a15
michael kostal <michael.kostal@united.com>
2023-10-03 20:29:58 -0500

    add reset

--- src/components/EndGame/EndGame.js
+++ src/components/EndGame/EndGame.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-function EndGame({ winner, guesses, answer }){
+function PlayAgain({handlePlayAgain}){
+  return <button style={{'textDecoration':'underline'}} onClick={(event) => {
+    handlePlayAgain();
+  }}>Play again</button>
+}
+
+function EndGame({ winner, guesses, answer, handlePlayAgain }){
   const attempts = guesses.length;
   if (winner){
     return <div className="happy banner">
-    <p>Woohoo! It only took <strong>{attempts} guess{attempts === 1 ? '' : 'es'}</strong></p>
+      <p>Woohoo! It only took <strong>{attempts} guess{attempts === 1 ? '' : 'es'}</strong>. <PlayAgain handlePlayAgain={handlePlayAgain} /></p>
     </div>
   } else {
     return <div className="sad banner">
-    <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+      <p>Sorry, the correct answer is <strong>{answer}</strong>. <PlayAgain handlePlayAgain={handlePlayAgain} /></p>
     </div>
   }
 
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -11,16 +11,19 @@ import Keyboard from '../Keyboard';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
   const [gameOver, setGameOver] = React.useState(false);
   const [winner, setWinner] = React.useState(false);
   const [tentativeGuess, setTentativeGuess] = React.useState('');
-
+  const [answer, setAnswer] = React.useState(() => {
+    const initialAnswer = sample(WORDS);
+    console.info({ initialAnswer });
+    return initialAnswer;
+  })
   function handleSubmitGuess(tentativeGuess) {
     const nextGuesses = [...guesses, tentativeGuess];
     const attempts = nextGuesses.length;
@@ -32,12 +35,19 @@ function Game() {
       setGameOver(true);
     }
   }
-
+  function handlePlayAgain(){
+    const nextAnswer = sample(WORDS);
+    console.info({ nextAnswer });
+    setAnswer(nextAnswer);
+    setGameOver(false);
+    setWinner(false);
+    setGuesses([]);
+  }
   return <>
     <GuessResults guesses={guesses} answer={answer} />
     <GuessInput handleSubmitGuess={handleSubmitGuess} gameOver={gameOver} tentativeGuess={tentativeGuess} setTentativeGuess={setTentativeGuess} />
     <Keyboard handleSubmitGuess={handleSubmitGuess} guesses={guesses} answer={answer} tentativeGuess={tentativeGuess} setTentativeGuess={setTentativeGuess} />
-    {gameOver && <EndGame winner={winner} guesses={guesses} answer={answer} />}
+    {gameOver && <EndGame winner={winner} guesses={guesses} answer={answer} handlePlayAgain={handlePlayAgain} />}
   </>
 }
 
